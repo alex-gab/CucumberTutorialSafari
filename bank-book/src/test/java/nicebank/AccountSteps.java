699959605d15babc181b1e30ed2a6b42aa05462d
Alex Dragoi <dragoi.alex@gmail.com>
2016-06-05 16:14:06 +0300

    [#1]: add web user interface, book version

---
+++ bank-book/src/test/java/nicebank/AccountSteps.java
@@ -0,0 +1,40 @@
+/***
+ * Excerpted from "The Cucumber for Java Book",
+ * published by The Pragmatic Bookshelf.
+ * Copyrights apply to this code. It may not be used to create training material, 
+ * courses, books, articles, and the like. Contact us if you are in doubt.
+ * We make no guarantees that this code is fit for any purpose. 
+ * Visit http://www.pragmaticprogrammer.com/titles/srjcuc for more book information.
+***/
+package nicebank;
+
+import cucumber.api.Transform;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import org.junit.Assert;
+import support.KnowsTheDomain;
+import transforms.MoneyConverter;
+
+public class AccountSteps {
+    KnowsTheDomain helper;
+
+    public AccountSteps(KnowsTheDomain helper) {
+        this.helper = helper;
+    }
+      
+    @Given("^my account has been credited with (\\$\\d+\\.\\d+)$")
+    public void myAccountHasBeenCreditedWith$(
+                                @Transform(MoneyConverter.class) Money amount) 
+                                                              throws Throwable {
+        helper.getMyAccount().credit(amount);
+    }
+
+    @Then("^the balance of my account should be (\\$\\d+\\.\\d+)$")
+    public void theBalanceOfMyAccountShouldBe$(
+                                @Transform(MoneyConverter.class) Money amount) 
+                                                              throws Throwable {
+
+        Assert.assertEquals("Incorrect account balance -",
+                                      amount, helper.getMyAccount().getBalance()); 
+    }
+}
---
+++ bank-book/src/test/java/nicebank/TellerSteps.java
@@ -0,0 +1,26 @@
+/***
+ * Excerpted from "The Cucumber for Java Book",
+ * published by The Pragmatic Bookshelf.
+ * Copyrights apply to this code. It may not be used to create training material, 
+ * courses, books, articles, and the like. Contact us if you are in doubt.
+ * We make no guarantees that this code is fit for any purpose. 
+ * Visit http://www.pragmaticprogrammer.com/titles/srjcuc for more book information.
+***/
+package nicebank;
+
+import cucumber.api.java.en.When;
+import support.KnowsTheDomain;
+
+public class TellerSteps {
+
+  KnowsTheDomain helper;
+
+  public TellerSteps(KnowsTheDomain helper) {
+      this.helper = helper;
+  }
+      
+  @When("^I withdraw \\$(\\d+)$")
+  public void iWithdraw$(int amount) throws Throwable {
+      helper.getTeller().withdrawFrom(helper.getMyAccount(), amount);
+  }
+}
